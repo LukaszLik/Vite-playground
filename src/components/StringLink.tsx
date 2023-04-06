5c55672847f1953cbf02cdd37f6808a0eadc8960
Łukasz Lik <lik.lukasz2@gmail.com>
2023-04-07 01:52:50 +0200

    Add text fields to boxes

--- src/components/StringLink.tsx
+++ src/components/StringLink.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import {TextField} from "@mui/material";
+import React, {useEffect} from "react";
 import "./StringLink.scss";
 
 export default function StringLink() {
@@ -9,39 +10,104 @@ export default function StringLink() {
     let objectsList: JSX.Element[] = [];
 
     list.map((str, idx) => {
-      objectsList.push(
-        <>
-          <button
-            id={idx.toString()}
-            onClick={addLink}
-            className="addLink-btn"
-          />
-          <div className="single-link">
-            <p>{str}</p>
-          </div>
-        </>
-      );
+      idx === list.length - 1
+        ? objectsList.push(
+            <>
+              <button
+                id={idx.toString()}
+                onClick={addLink}
+                className="addLink-btn"
+              />
+              <div className="single-link">
+                <TextField
+                  className="input"
+                  id={idx.toString()}
+                  placeholder={list[idx]}
+                  size="small"
+                  variant="standard"
+                  onChange={updateList}
+                />
+              </div>
+              <button
+                id={(idx + 1).toString()}
+                onClick={addLink}
+                className="addLink-btn"
+              />
+            </>
+          )
+        : objectsList.push(
+            <>
+              <button
+                id={idx.toString()}
+                onClick={addLink}
+                className="addLink-btn"
+              />
+              <div className="single-link">
+                <TextField
+                  className="input"
+                  id={idx.toString()}
+                  placeholder={list[idx]}
+                  size="small"
+                  variant="standard"
+                  onChange={updateList}
+                />
+                {/*<p>{str}</p>*/}
+              </div>
+            </>
+          );
     });
-    objectsList.push(
-      <button
-        id={objectsList.length.toString()}
-        onClick={addLink}
-        className="addLink-btn"
-      />
-    );
-    setLinkedList(objectsList);
-  }, []);
 
+    setLinkedList(objectsList);
+  }, [list]);
+
+  useEffect(() => {}, [linkedList]);
+
+  function updateList(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    console.log(`${e.target.value} ${e.target.id}`);
+
+    let newList = list.map((value, idx) => {
+      if (Number(e.target.id) === idx) {
+        value = e.target.value;
+      }
+      return value;
+    });
+
+    console.log(newList);
+    setList(newList);
+  }
   function addLink(event: React.MouseEvent<HTMLButtonElement>) {
-    let newList = [...list];
-    console.log(event.currentTarget.id);
+    let newList = [];
+
+    if (event.currentTarget.id === "0") {
+      newList.push("x");
+      list.map((str, idx) => newList.push(str));
+      setList(newList);
+    } else if (event.currentTarget.id === list.length.toString()) {
+      list.map((str, idx) => newList.push(str));
+      newList.push("x");
+      setList(newList);
+    } else {
+      const len = list.length;
+      newList = list.slice(0, Number(event.currentTarget.id));
+      newList.push("x");
+      list.slice(Number(event.currentTarget.id), len).map((elem, idx) => {
+        newList.push(elem);
+      });
+      setList(newList);
+    }
   }
 
   return (
-    <div className="main-div">
-      {linkedList.map((obj, idx) => {
-        return obj;
-      })}
-    </div>
+    <>
+      <div className="main-div">
+        {linkedList.map((obj, idx) => {
+          return obj;
+        })}
+      </div>
+      <p className="counter">{linkedList.length}</p>
+      <p>
+        Total string: {list.reduce((str: string, elem) => (str += elem), "")}
+      </p>
+    </>
   );
 }
